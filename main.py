3a920a7f4eb1f0f9804318964627c0a020251946
mojitog <mh.mojallal@gmail.com>
2025-10-21 22:17:23 +0200

    task class completed

---
+++ main.py
@@ -0,0 +1,8 @@
+class Task:
+    def __init__(self, name, definition, prio):
+        self.name = name
+        self.definition = definition
+        self.prio = prio
+
+
+t = Task("Dinner", "Cooking proper meal for dinner, then just eat it", "high")
