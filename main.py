2e9a3e6509bb7cabf91d6a0e031e164fb75669ff
mojitog <mh.mojallal@gmail.com>
2025-10-23 19:06:52 +0200

    to do list class and few methods

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ class Task:
         self.name = name
         self.definition = definition
         self.prio = prio
+    def __str__(self):
+        s = f"{self.name} | {self.definition} | {self.prio}"
+        return s   
 
 
-t = Task("Dinner", "Cooking proper meal for dinner, then just eat it", "high")
+class To_do_list:
+    """
+    The basic to_do_list class that needs to get more efficient and functional
+    """
+    def __init__(self, task_list):
+        self.task_list = task_list
+    def add_task(self, task):
+        self.task_list.append(task)
+    def rmv_task(self, task):
+        self.task_list.remove(task)
+    def show_task(self):
+        print(self.task_list)    
+
+
+t1 = Task("Dinner", "Cooking proper meal for dinner, then just eat it", "high")
+t2 = Task("Chess Club", "Attend the chess club and have fun", "medium")
+print(t1)
+task_list = [t1, t2]
+print(task_list)
+#print(task_list)
+
+mylist = To_do_list(task_list)
+
+mylist.show_task()
